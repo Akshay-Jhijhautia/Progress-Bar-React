1ed45b83f48342d559c8bf496e1cb890725cb7e4
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2025-04-05 12:34:44 +0530

    added a dynamic progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import ProgressBar from "./ProgressBar";
+import { useState } from "react";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
   return (
     <div className="App">
       <h1>Progress Bar</h1>
-      <ProgressBar progress={50} />
+      <ProgressBar progress={90} />
     </div>
   );
 }
--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 const ProgressBar = ({ progress }) => {
-  return <div>{progress}%</div>;
+  return (
+    <div className="outer">
+      <div className="inner" style={{ width: `${progress}%` }}>
+        {progress}%
+      </div>
+    </div>
+  );
 };
 
 export default ProgressBar;
